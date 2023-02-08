475791e686fefa57c262de1be0d39f1dc7659e16
Finley Ge <m13203533462@163.com>
2023-02-08 21:06:16 +0800

    feat:pdg2pdf

---
+++ pdg2pdf.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python3
+# transfer .pdg to pdf
+
+import sys
+import zipfile
+from PIL import Image
+import os
+def pdg2pdf(pdgfile:zipfile.ZipFile, target: str):
+    pdgfile.extractall('./temp')
+    pdgfile.close()
+    toc:list[str] = []
+    content:list[str] = []
+    cov1:str = ''
+    cov2:str = ''
+    bok:list[str] = []
+    fow:list[str] = []
+    leg:list[str] = []
+    for file in os.listdir('./temp'):
+        if file.endswith('.pdg'):
+            if file.startswith('!'):
+                toc.append(file)
+            elif file.startswith('fow'):
+                fow.append(file)
+            elif file.startswith('leg'):
+                leg.append(file)
+            elif file == 'cov001.pdg':
+                cov1 = 'cov001.pdg'
+            elif file == 'cov002.pdg':
+                cov2  = 'cov002.pdg'
+            elif file.startswith('bok'):
+                bok.append(file)
+            else:
+                content.append(file)
+    content.sort()
+    fow.sort()
+    leg.sort()
+    toc.sort()
+    image_list:list[Image.Image] = []
+
+    if cov1 != '':
+        image_list.append(Image.open('./temp/' + cov1).convert('RGB'))
+
+    for b in bok:
+        image_list.append(Image.open('./temp/' + b).convert('RGB'))
+
+    for l in leg:
+        image_list.append(Image.open('./temp/' + l).convert('RGB'))
+
+    for f in fow:
+        image_list.append(Image.open('./temp/' + f).convert('RGB'))
+
+    for t in toc:
+        image_list.append(Image.open('./temp/' + t).convert('RGB'))
+
+    for c in content:
+        image_list.append(Image.open('./temp/' + c).convert('RGB'))
+
+    if cov2 != '':
+        image_list.append(Image.open('./temp/' + cov2).convert('RGB'))
+    
+    image_list[0].save(target, save_all=True, append_images=image_list[1:])
+
+    # delele all temp file 
+    for file in os.listdir('./temp'):
+        os.remove('./temp/' + file)
+    os.rmdir('./temp')
+
+def main():
+    param:list[str] = sys.argv
+    if len(param) != 3:
+        print("Usage: python pdg2pdf.py filename output.pdf")
+        return
+    file_path:str = param[1]
+    if file_path[-4:] != ".zip":
+        print("Usage: Input FileType must be .zip")
+        return
+    pdgfile:zipfile.ZipFile = zipfile.ZipFile(file_path)
+    pdg2pdf(pdgfile, param[2])
+
+if __name__ == '__main__':
+    main()
